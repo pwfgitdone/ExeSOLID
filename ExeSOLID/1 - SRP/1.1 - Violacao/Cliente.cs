4f634326436f21cbab28dd9da4102dd1ac388d1f
Daniel Cardoso Carneiro <ddrdan7@hotmail.com>
2023-08-04 20:57:59 -0300

    Initial

---
+++ ExeSOLID/1 - SRP/1.1 - Violacao/Cliente.cs
@@ -0,0 +1,17 @@
+﻿namespace ExeSOLID.SRP.Violacao
+{
+    public class Cliente
+    {
+        public int ClienteId { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string CPF { get; set; }
+        public DateTime DataCadastro { get; set; }
+
+        public string AdicionarCliente()
+        {
+
+        }
+
+    }
+}

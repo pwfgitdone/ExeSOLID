b0156616abb66feb18af1c893187aed3b396ba96
Daniel Cardoso Carneiro <ddrdan7@hotmail.com>
2023-08-04 22:09:02 -0300

    LSP

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ExeSOLID.LSP.Violacao;
+
+Console.WriteLine("Hello, World!");
+
+void ObterAreaRetangulo(Retangulo ret)
+{
+    Console.WriteLine("Calculo da área do Retangulo");
+    Console.WriteLine(ret.Altura.ToString() + " * " + ret.Largura.ToString());
+    Console.WriteLine(ret.Area);
+    Console.ReadKey();
+}
+
+
+var quad = new Quadrado()
+{
+    Altura = 10,
+    Largura = 5
+};
+
+ObterAreaRetangulo(quad);
---
+++ ExeSOLID/3 - LSP/3.1 - Violacao/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExeSOLID.LSP.Violacao
+{
+    public class Program
+    {
+        private static void ObterAreaRetangulo(Retangulo ret)
+        {
+            Console.WriteLine("Calculo da área do Retangulo");
+            Console.WriteLine(ret.Altura.ToString() + " * " + ret.Largura.ToString());
+            Console.WriteLine(ret.Area);
+            Console.ReadKey();
+        }
+
+        private static void Main()
+        {
+            var quad = new Quadrado()
+            {
+                Altura = 10,
+                Largura = 5
+            };
+
+            ObterAreaRetangulo(quad);
+        }
+        
+    }
+}
